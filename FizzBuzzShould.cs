545ddca86e3adf4a8cb841064dcc7b9dccf4967e
Romain Deneau <rdeneau@yahoo.fr>
2018-10-20 10:45:13 +0200

    Test 5 : refactor 2 : `number.FizzBuzz()`

--- FizzBuzzShould.cs
+++ FizzBuzzShould.cs
@@ -10,7 +10,7 @@ namespace DemoFizzBuzz
         [InlineData(2)]
         public void Return_Given_Number_Stringifyed(int number)
         {
-            FizzBuzz.Of(number).Should().Be(number.ToString());
+            number.FizzBuzz().Should().Be(number.ToString());
         }
 
         [Theory]
@@ -18,22 +18,22 @@ namespace DemoFizzBuzz
         [InlineData(6)]
         public void Return_Fizz_Given_Multiple_Of_3(int number)
         {
-            FizzBuzz.Of(number).Should().Be("Fizz");
+            number.FizzBuzz().Should().Be("Fizz");
         }
 
         [Theory]
         [InlineData(5)]
         public void Return_Buzz_Given_Multiple_Of_5(int number)
         {
-            FizzBuzz.Of(number).Should().Be("Buzz");
+            number.FizzBuzz().Should().Be("Buzz");
         }
     }
 
     // Prod
 
-    public static class FizzBuzz
+    public static class NumberExtensions
     {
-        public static string Of(int number)
+        public static string FizzBuzz(this int number)
         {
             if (number.IsMultipleOf(3)) return "Fizz";
             if (number.IsMultipleOf(5)) return "Buzz";
