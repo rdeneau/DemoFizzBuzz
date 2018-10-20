3004d165991a81aa82544e67a985f901a91d2397
Romain Deneau <rdeneau@yahoo.fr>
2018-10-20 10:28:17 +0200

    Test 2 : refactor #2 : reveal pattern more clearly

--- FizzBuzzShould.cs
+++ FizzBuzzShould.cs
@@ -24,8 +24,8 @@ namespace DemoFizzBuzz
     {
         public static string Of(int number)
         {
-            if (number == 2) return "2";
-            if (number == 1) return "1";
+            if (number == 2) return $"{number}";
+            if (number == 1) return $"{number}";
             return "";
         }
     }
